c469753d69577118e5547ae38de1890f1d053701
Daehan <dh16931@gmail.com>
2021-09-01 10:34:53 +0900

    feat: change Oauth2.0 to service account

--- flask/core/google_drive_api.py
+++ flask/core/google_drive_api.py
@@ -1,9 +1,6 @@
 import os.path
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-
+from oauth2client.service_account import ServiceAccountCredentials
 
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
@@ -11,21 +8,8 @@ creds = None
 
 def init_google_service():
     global creds
-    if os.path.exists('token.json'):
-        print("google api auth token exists")
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'client_secret.json', SCOPES)
-            creds = flow.run_local_server(port=8080)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:    
-            print("update google api auth token")
-            token.write(creds.to_json())
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+            'client_secret.json', scopes=SCOPES)
 
 
 def get_folder_id(folder_name='furfellas'):
@@ -43,7 +27,7 @@ def get_folder_id(folder_name='furfellas'):
     return False
 
 def get_service():
-    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    global creds
     service = build('drive', 'v3', credentials=creds)
     return service
 

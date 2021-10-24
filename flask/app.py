c62f442bf8f15ee8ae66b61b1e4a6ff02373e255
daehan <dh16931@gmail.com>
2021-10-24 23:14:15 +0900

    feat: add orm to replace sql query db connection

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,19 @@
+import os
 import traceback
 from flask import Flask
 from flask_cors import CORS
+from dotenv import load_dotenv
 
+from core.database import db
 from resources import blueprint as api
 from core.google_drive_api import init_google_service
-from core.db import init_db
+from core.db import init_db  ## TODO: delete and replace it to database(sqlalchemy) db
+
+
+APP_ROOT = os.path.join(os.path.dirname(__file__), "..")
+dotenv_path = os.path.join(APP_ROOT, ".env")
+load_dotenv(dotenv_path)
+
 
 def init_settings():
     try:
@@ -13,16 +22,21 @@ def init_settings():
     except:
         traceback.print_exc()
 
+
+def set_db(app):
+    with app.app_context():
+        db.create_all()
+
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = "ssseetrr"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False        
+    app.config["SECRET_KEY"] = "ssseetrr"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
     CORS(app, resources={r"/api/*": {"origins": "*"}})
-    
+    db.init_app(app)
     init_settings()
-
-    app.register_blueprint(api, url_prefix='/api')
-
+    app.register_blueprint(api, url_prefix="/api")
 
     return app

558e2f960f7a7c1fbed80ee8ee2b26e2c4046a4a
daehan <dh16931@gmail.com>
2021-10-27 08:38:03 +0900

    test: create todo with default value for repeat, finish time

--- flask/tests/test_todo.py
+++ flask/tests/test_todo.py
@@ -15,9 +15,7 @@ def test_get_todo_by_id(client, tables):
     headers = {"Content-Type": mimetype, "Accept": mimetype}
     data = {
         "task": "wake up",
-        "repeat_interval": "1d",
         "start_datetime": now.isoformat(),
-        "finish_datetime": (now + relativedelta(months=6)).isoformat(),
     }
     url = "/api/todos/"
     response = client.post(url, data=json.dumps(data), headers=headers)
@@ -25,16 +23,32 @@ def test_get_todo_by_id(client, tables):
     assert client.get(f"/api/todos/{id}").status_code == 200
 
 
-def test_create_todos(client, tables):
+def test_create_todo_without_repeat_interval(client, tables):
     now = datetime.now()
 
     mimetype = "application/json"
     headers = {"Content-Type": mimetype, "Accept": mimetype}
     data = {
         "task": "wake up",
-        "repeat_interval": "1d",
         "start_datetime": now.isoformat(),
         "finish_datetime": (now + relativedelta(months=6)).isoformat(),
+    }
+    url = "/api/todos/"
+
+    response = client.post(url, data=json.dumps(data), headers=headers)
+
+    assert response.status_code == 201
+    assert isinstance(json.loads(response.data.decode("utf-8"))["result"], int)
+
+
+def test_create_todo_without_finish_datetime(client, tables):
+    now = datetime.now()
+
+    mimetype = "application/json"
+    headers = {"Content-Type": mimetype, "Accept": mimetype}
+    data = {
+        "task": "wake up",
+        "start_datetime": now.isoformat(),
     }
     url = "/api/todos/"
 
@@ -49,7 +63,6 @@ def test_create_todo_without_task(client, tables):
     mimetype = "application/json"
     headers = {"Content-Type": mimetype, "Accept": mimetype}
     data = {
-        "repeat_interval": "1d",
         "start_datetime": now.isoformat(),
         "finish_datetime": (now + relativedelta(months=6)).isoformat(),
     }
@@ -66,9 +79,7 @@ def test_delete_todo_by_id(client, tables):
     headers = {"Content-Type": mimetype, "Accept": mimetype}
     data = {
         "task": "wake up",
-        "repeat_interval": "1d",
         "start_datetime": now.isoformat(),
-        "finish_datetime": (now + relativedelta(months=6)).isoformat(),
     }
     url = "/api/todos/"
 
@@ -86,3 +97,18 @@ def test_get_todo_with_wrong_identifier(client, tables):
 def test_delete_todo_with_wrong_identifier(client, tables):
     assert client.delete(f"/api/todos/34902347903470").status_code == 404
     assert client.delete(f"/api/todos/asfwer").status_code == 404
+
+
+# def test_create_toto_with_todo_children(client, tables):
+#     now = datetime.now()
+#     mimetype = "application/json"
+#     headers = {"Content-Type": mimetype, "Accept": mimetype}
+#     data = {
+#         "task": "wake up",
+#         "repeat_interval": "1m",
+#         "start_datetime": now.isoformat(),
+#         "finish_datetime": (now + relativedelta(months=6)).isoformat(),
+#     }
+#     url = "/api/todos/"
+
+#     client.post(url, data=json.dumps(data), headers=headers)

f3ecdf8f974b93cb273bbe3bafc12b93080446ee
daehan <dh16931@gmail.com>
2021-10-27 08:19:56 +0900

    test:  get and delete todo for not existing identifier

--- flask/tests/test_todo.py
+++ flask/tests/test_todo.py
@@ -58,3 +58,31 @@ def test_create_todo_without_task(client, tables):
     response = client.post(url, data=json.dumps(data), headers=headers)
 
     assert response.status_code == 500
+
+
+def test_delete_todo_by_id(client, tables):
+    now = datetime.now()
+    mimetype = "application/json"
+    headers = {"Content-Type": mimetype, "Accept": mimetype}
+    data = {
+        "task": "wake up",
+        "repeat_interval": "1d",
+        "start_datetime": now.isoformat(),
+        "finish_datetime": (now + relativedelta(months=6)).isoformat(),
+    }
+    url = "/api/todos/"
+
+    response = client.post(url, data=json.dumps(data), headers=headers)
+    id = json.loads(response.data.decode("utf-8"))["result"]
+
+    assert client.delete(f"/api/todos/{id}").status_code == 204
+
+
+def test_get_todo_with_wrong_identifier(client, tables):
+    assert client.get(f"/api/todos/34902347903470").status_code == 404
+    assert client.get(f"/api/todos/asfwer").status_code == 404
+
+
+def test_delete_todo_with_wrong_identifier(client, tables):
+    assert client.delete(f"/api/todos/34902347903470").status_code == 404
+    assert client.delete(f"/api/todos/asfwer").status_code == 404

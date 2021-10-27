f614e89bb21a225a2469930e1c0a8234f0daf565
daehan <dh16931@gmail.com>
2021-10-27 22:22:34 +0900

    test: todo group for todo parent, todo for todo children

--- flask/tests/test_todo.py
+++ flask/tests/test_todo.py
@@ -8,7 +8,7 @@ def test_get_todos(client, tables):
     assert client.get("/api/todos/").status_code == 200
 
 
-def test_get_todo_by_id(client, tables):
+def test_get_todos_of_parent(client, tables):
     now = datetime.now()
 
     mimetype = "application/json"
@@ -17,98 +17,9 @@ def test_get_todo_by_id(client, tables):
         "task": "wake up",
         "start_datetime": now.isoformat(),
     }
-    url = "/api/todos/"
+    url = "/api/todo-groups/"
     response = client.post(url, data=json.dumps(data), headers=headers)
-    id = json.loads(response.data.decode("utf-8"))["result"]
-    assert client.get(f"/api/todos/{id}").status_code == 200
+    parent_id = json.loads(response.data.decode("utf-8"))["result"]
+    print(parent_id)
 
-
-def test_create_todo_without_repeat_interval(client, tables):
-    now = datetime.now()
-
-    mimetype = "application/json"
-    headers = {"Content-Type": mimetype, "Accept": mimetype}
-    data = {
-        "task": "wake up",
-        "start_datetime": now.isoformat(),
-        "finish_datetime": (now + relativedelta(months=6)).isoformat(),
-    }
-    url = "/api/todos/"
-
-    response = client.post(url, data=json.dumps(data), headers=headers)
-
-    assert response.status_code == 201
-    assert isinstance(json.loads(response.data.decode("utf-8"))["result"], int)
-
-
-def test_create_todo_without_finish_datetime(client, tables):
-    now = datetime.now()
-
-    mimetype = "application/json"
-    headers = {"Content-Type": mimetype, "Accept": mimetype}
-    data = {
-        "task": "wake up",
-        "start_datetime": now.isoformat(),
-    }
-    url = "/api/todos/"
-
-    response = client.post(url, data=json.dumps(data), headers=headers)
-
-    assert response.status_code == 201
-    assert isinstance(json.loads(response.data.decode("utf-8"))["result"], int)
-
-
-def test_create_todo_without_task(client, tables):
-    now = datetime.now()
-    mimetype = "application/json"
-    headers = {"Content-Type": mimetype, "Accept": mimetype}
-    data = {
-        "start_datetime": now.isoformat(),
-        "finish_datetime": (now + relativedelta(months=6)).isoformat(),
-    }
-    url = "/api/todos/"
-
-    response = client.post(url, data=json.dumps(data), headers=headers)
-
-    assert response.status_code == 500
-
-
-def test_delete_todo_by_id(client, tables):
-    now = datetime.now()
-    mimetype = "application/json"
-    headers = {"Content-Type": mimetype, "Accept": mimetype}
-    data = {
-        "task": "wake up",
-        "start_datetime": now.isoformat(),
-    }
-    url = "/api/todos/"
-
-    response = client.post(url, data=json.dumps(data), headers=headers)
-    id = json.loads(response.data.decode("utf-8"))["result"]
-
-    assert client.delete(f"/api/todos/{id}").status_code == 204
-
-
-def test_get_todo_with_wrong_identifier(client, tables):
-    assert client.get(f"/api/todos/34902347903470").status_code == 404
-    assert client.get(f"/api/todos/asfwer").status_code == 404
-
-
-def test_delete_todo_with_wrong_identifier(client, tables):
-    assert client.delete(f"/api/todos/34902347903470").status_code == 404
-    assert client.delete(f"/api/todos/asfwer").status_code == 404
-
-
-def test_create_toto_with_todo_children(client, tables):
-    now = datetime.now()
-    mimetype = "application/json"
-    headers = {"Content-Type": mimetype, "Accept": mimetype}
-    data = {
-        "task": "wake up",
-        "repeat_interval": "1m",
-        "start_datetime": now.isoformat(),
-        "finish_datetime": (now + relativedelta(months=6)).isoformat(),
-    }
-    url = "/api/todos/"
-
-    client.post(url, data=json.dumps(data), headers=headers)
+    assert client.get(f"/api/todos/?parent_id={parent_id}").status_code == 200

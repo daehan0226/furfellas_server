a60b0170b4b40ea6144f566471f3e00b2efe1405
daehan <dh16931@gmail.com>
2021-10-29 07:48:39 +0900

    test: change test mysql db to sqlite

--- flask/tests/conftest.py
+++ flask/tests/conftest.py
@@ -1,11 +1,10 @@
 import os
 import pytest
-from app import create_app
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from core.models import Action, Location, TodoParent, TodoChildren
-
 from dotenv import load_dotenv
+
+from app import create_app
+from core.database import db
 
 APP_ROOT = os.path.join(os.path.dirname(__file__), "..")  # refers to application_top
 dotenv_path = os.path.join(APP_ROOT, ".env")
@@ -16,50 +15,20 @@ load_dotenv(dotenv_path)
 def client():
     app = create_app()
     app.config["TESTING"] = True
+    app.testing = True
+    with app.test_client() as client:
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 
-    with app.app_context():
-        with app.test_client() as client:
-            app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
-                "SQLALCHEMY_TEST_DATABASE_URI"
-            )
+        with app.app_context():
+            db.create_all()
             yield client
 
 
 @pytest.fixture()
 def db_engine():
     """yields a SQLAlchemy engine which is suppressed after the test session"""
-    engine_ = create_engine(os.getenv("SQLALCHEMY_TEST_DATABASE_URI"), echo=True)
+    engine_ = create_engine("sqlite:///test.db", echo=True)
 
     yield engine_
 
     engine_.dispose()
-
-
-@pytest.fixture()
-def tables(db_engine):
-    Action.metadata.create_all(db_engine)
-    Location.metadata.create_all(db_engine)
-    TodoParent.metadata.create_all(db_engine)
-    TodoChildren.metadata.create_all(db_engine)
-    yield
-    # TODO: BUG - TEST STOPS - Lock
-    # Action.metadata.drop_all(db_engine)
-    # Location.metadata.drop_all(db_engine)
-    # TodoParent.metadata.drop_all(db_engine)
-    # TodoChildren.metadata.drop_all(db_engine)
-
-
-@pytest.fixture()
-def db_session_factory(db_engine):
-    """returns a SQLAlchemy scoped session factory"""
-    return scoped_session(sessionmaker(bind=db_engine))
-
-
-@pytest.fixture()
-def db_session(db_session_factory):
-    """yields a SQLAlchemy connection which is rollbacked after the test"""
-    session_ = db_session_factory()
-
-    yield session_
-    session_.rollback()
-    session_.close()
--- flask/tests/test_db.py
+++ flask/tests/test_db.py
@@ -1,67 +1,10 @@
+import sqlalchemy
 from core.models import Action, Location, TodoParent, TodoChildren
 
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
 
-
-def test_check_if_tables_created(db_engine, tables):
-    assert Action.__tablename__ in db_engine.table_names()
-    assert Location.__tablename__ in db_engine.table_names()
-    assert TodoParent.__tablename__ in db_engine.table_names()
-    assert TodoChildren.__tablename__ in db_engine.table_names()
-
-
-def test_post_action(db_session, tables):
-    db_session.add(Action("testing-action"))
-    db_session.commit()
-
-
-def test_delete_action(db_session, tables):
-    db_session.query(Action).filter(Action.name == "testing-action").delete()
-    db_session.commit()
-
-
-def test_post_location(db_session, tables):
-    db_session.add(Location("testing-location"))
-    db_session.commit()
-
-
-def test_delete_action(db_session, tables):
-    db_session.query(Location).filter(Location.name == "testing-location").delete()
-    db_session.commit()
-
-
-def test_create_todo_parent(db_session, tables):
-    now = datetime.now()
-    db_session.add(
-        TodoParent(
-            "Have a bath",
-            "1m",
-            now,
-            now + relativedelta(months=6),
-        )
-    )
-    db_session.commit()
-
-
-def test_delete_todo_parent(db_session, tables):
-    now = datetime.now()
-    db_session.add(
-        TodoParent(
-            "Take medicine",
-            "1m",
-            now,
-            now + relativedelta(months=6),
-        )
-    )
-    db_session.query(TodoParent).filter(TodoParent.task == "Take medicine").delete()
-    db_session.commit()
-
-
-def test_create_todo_children(db_session, tables):
-    now = datetime.now()
-    todo_parent = (
-        db_session.query(TodoParent).filter(TodoParent.task == "Have a bath").first()
-    )
-    db_session.add(TodoChildren(now, todo_parent.id))
-    db_session.commit()
+def test_check_if_tables_created(db_engine):
+    insp = sqlalchemy.inspect(db_engine)
+    assert insp.dialect.has_table(db_engine.connect(), Action.__tablename__)
+    assert insp.dialect.has_table(db_engine.connect(), Location.__tablename__)
+    assert insp.dialect.has_table(db_engine.connect(), TodoParent.__tablename__)
+    assert insp.dialect.has_table(db_engine.connect(), TodoChildren.__tablename__)

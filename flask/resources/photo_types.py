21e08d11c891e6d6345bd990147c9ff8b3ee498e
daehan <dh16931@gmail.com>
2021-11-02 08:42:46 +0900

    feat: add status constants in photo types

--- flask/resources/photo_types.py
+++ flask/resources/photo_types.py
@@ -1,20 +1,25 @@
 import traceback
+import sqlalchemy
 from flask_restplus import Namespace, reqparse
 
+from core.constants import response_status
 from core.resource import CustomResource
 from core.models import PhotoType as PhotoTypeModel
 from core.database import db
 
 api = Namespace("photo-types", description="photo types related operations")
 
-parser_post = reqparse.RequestParser()
-parser_post.add_argument("name", type=str, required=True, help="photo type name")
+parser_name = reqparse.RequestParser()
+parser_name.add_argument("name", type=str, required=True, help="photo type name")
 
 
 def create_photo_type(name):
-    photo_type = PhotoTypeModel(name)
-    photo_type.create()
-    return photo_type.id
+    try:
+        photo_type = PhotoTypeModel(name)
+        photo_type.create()
+        return photo_type, ""
+    except sqlalchemy.exc.IntegrityError as e:
+        return False, f"Photo type '{name}' already exsits."
 
 
 def get_photo_type(id):
@@ -23,17 +28,13 @@ def get_photo_type(id):
 
 
 def update_photo_type(id, name):
-    try:
-        photo_type = PhotoTypeModel.query.filter_by(id=id).first()
-        photo_type.name = name
-        db.session.commit()
-        return True
-    except:
-        return False
+    photo_type = PhotoTypeModel.query.filter_by(id=id).first()
+    photo_type.name = name
+    db.session.commit()
 
 
 def delete_photo_type(id):
-    return PhotoTypeModel.query.filter_by(id=id).delete()
+    PhotoTypeModel.query.filter_by(id=id).delete()
 
 
 def get_photo_types():
@@ -44,44 +45,56 @@ def get_photo_types():
 class PhotoTypes(CustomResource):
     def get(self):
         try:
-            return self.send(status=200, result=get_photo_types())
+            return self.send(status=response_status.SUCCESS, result=get_photo_types())
         except:
             traceback.print_exc()
-            return self.send(status=500)
+            return self.send(status=response_status.SEVER_ERROR)
 
-    @api.expect(parser_post)
+    @api.expect(parser_name)
     def post(self):
         try:
-            args = parser_post.parse_args()
-            result = create_photo_type(args["name"])
-            if result is None:
-                return self.send(status=500)
-            return self.send(status=201)
+            args = parser_name.parse_args()
+            result, message = create_photo_type(args["name"])
+            if result:
+                return self.send(status=response_status.CREATED, result=result.id)
+            return self.send(status=response_status.FAIL, message=message)
+
         except:
             traceback.print_exc()
-            return self.send(status=500)
+            return self.send(status=response_status.SEVER_ERROR)
 
 
 @api.route("/<int:id_>")
 @api.param("id_", "The photo type identifier")
 class PhotoType(CustomResource):
-    @api.expect(parser_post)
-    def put(self, id_):
+    def get(self, id_):
         try:
-            args = parser_post.parse_args()
-            result = update_photo_type(id_, args["name"])
-            return self.send(status=204) if result else self.send(status=400)
+            photo_type = get_photo_type(id_)
+            if photo_type:
+                return self.send(status=response_status.SUCCESS, result=photo_type)
+            return self.send(status=response_status.NOT_FOUND)
         except:
             traceback.print_exc()
-            return self.send(status=500)
+            return self.send(status=response_status.SEVER_ERROR)
+
+    @api.expect(parser_name)
+    def put(self, id_):
+        try:
+            if get_photo_type(id_):
+                args = parser_name.parse_args()
+                update_photo_type(id_, args["name"])
+                return self.send(status=response_status.NO_CONTENT)
+            return self.send(status=response_status.NOT_FOUND)
+        except:
+            traceback.print_exc()
+            return self.send(status=response_status.SEVER_ERROR)
 
     def delete(self, id_):
         try:
-            result = delete_photo_type(id_)
-            if result is None:
-                return self.send(status=500)
-            return self.send(status=200)
-
+            if get_photo_type(id_):
+                delete_photo_type(id_)
+                return self.send(status=response_status.NO_CONTENT)
+            return self.send(status=response_status.NOT_FOUND)
         except:
             traceback.print_exc()
-            return self.send(status=500)
+            return self.send(status=response_status.SEVER_ERROR)

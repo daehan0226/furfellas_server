c0a999b34851d3a1f68664ffe745a8ec68769a46
daehan <dh16931@gmail.com>
2021-11-05 00:22:51 +0900

    style: make generate headers and response body as staticmethod

--- flask/core/resource.py
+++ flask/core/resource.py
@@ -10,19 +10,26 @@ class CustomResource(Resource):
         super().__init__(api, args, kwargs)
 
     def send(self, *args, **kwargs):
-        headers = {}
-        headers["Access-Control-Allow-Origin"] = "*"
-        headers["Access-Control-Allow-Headers"] = "*"
-        headers["Access-Control-Allow-Credentials"] = True
-
-        response_body = {}
-        response_body["result"] = kwargs.get("result")
-        response_body["message"] = kwargs.get("message")
+        response_body = self.gen_response_body(
+            result=kwargs.get("result"), message=kwargs.get("message")
+        )
 
         json_encode = json.JSONEncoder().encode
         return Response(
             json_encode(response_body),
             status=kwargs["status"],
-            headers=headers,
+            headers=self.gen_headers(),
             mimetype="application/json",
         )
+
+    @staticmethod
+    def gen_headers():
+        headers = {}
+        headers["Access-Control-Allow-Origin"] = "*"
+        headers["Access-Control-Allow-Headers"] = "*"
+        headers["Access-Control-Allow-Credentials"] = True
+        return headers
+
+    @staticmethod
+    def gen_response_body(result=None, message=None):
+        return {"result": result, "message": message}

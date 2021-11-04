547590a0ead9b8f3ca36190e381596d74fc04e3a
daehan <dh16931@gmail.com>
2021-11-04 23:44:44 +0900

    feat: add is_admin, is_manager in user model

--- flask/core/models/user.py
+++ flask/core/models/user.py
@@ -1,9 +1,14 @@
 import os
-from sqlalchemy import event
 from datetime import datetime
+from dotenv import load_dotenv
+from sqlalchemy import event
 from werkzeug.security import generate_password_hash, check_password_hash
 from core.database import db
 from core.models.base import BaseModel
+
+APP_ROOT = os.path.join(os.path.dirname(__file__), "..")
+dotenv_path = os.path.join(APP_ROOT, ".env")
+load_dotenv(dotenv_path)
 
 
 class User(BaseModel):
@@ -37,6 +42,16 @@ class User(BaseModel):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+    def is_admin(self):
+        if self.role_id == int(os.getenv("ADMIN_ROLE_ID")):
+            return True
+        return False
+
+    def is_manager(self):
+        if self.role_id == int(os.getenv("MANAGER_ROLE_ID")):
+            return True
+        return False
 
 
 @event.listens_for(User.__table__, "after_create")

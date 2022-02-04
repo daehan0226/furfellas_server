8bcf7b30e795c1e5f3f4e03ba90b3793fc07cd24
daehan <dh16931@gmail.com>
2022-02-05 00:55:47 +0900

    refactor: move model related functions from resources to model classmethod

--- flask/resources/actions.py
+++ flask/resources/actions.py
@@ -1,4 +1,3 @@
-import sqlalchemy
 from flask_restplus import Namespace, reqparse, Resource
 
 from core.response import (
@@ -7,7 +6,6 @@ from core.response import (
     login_required,
 )
 from core.models import Action as ActionModel
-from core.database import db
 from core.utils import set_doc_responses
 
 api = Namespace("actions", description="actions related operations")
@@ -32,33 +30,6 @@ parser_auth.add_argument(
 )
 
 
-def creat_action(name):
-    try:
-        action = ActionModel(name)
-        action.create()
-        return action, ""
-    except sqlalchemy.exc.IntegrityError as e:
-        return False, f"Action name '{name}' already exists."
-
-
-def get_actions(name=None) -> list:
-    if name is not None:
-        actions = ActionModel.query.filter(ActionModel.name.like(f"%{name}%"))
-    else:
-        actions = ActionModel.query.all()
-    return [action.serialize for action in actions]
-
-
-def get_action_by_name(name) -> object:
-    return ActionModel.query.filter_by(name=name).first()
-
-
-def update_action(id_, name):
-    action = ActionModel.query.get(id_)
-    action.name = name
-    db.session.commit()
-
-
 @api.route("/")
 class Actions(Resource, CustomeResponse):
     @api.doc(responses=set_doc_responses(200, 500))
@@ -67,7 +38,9 @@ class Actions(Resource, CustomeResponse):
     def get(self):
         """Get all actions with filter by name if given."""
         args = parser_search.parse_args()
-        return self.send(response_type="OK", result=get_actions(name=args["name"]))
+        return self.send(
+            response_type="OK", result=ActionModel.get_actions(name=args["name"])
+        )
 
     @api.doc(responses=set_doc_responses(200, 401, 403, 409, 500))
     @api.expect(parser_post, parser_auth)
@@ -77,7 +50,7 @@ class Actions(Resource, CustomeResponse):
         """Create a new action."""
         if kwargs["auth_user"].is_admin():
             args = parser_post.parse_args()
-            result, message = creat_action(args["name"])
+            result, message = ActionModel.creat_action(args["name"])
             if result:
                 return self.send(response_type="CREATED", result=result.id)
             return self.send(response_type="CONFLICT", additional_message=message)
@@ -108,12 +81,12 @@ class Action(Resource, CustomeResponse):
                 args = parser_post.parse_args()
                 if action.name == args["name"]:
                     return self.send(response_type="NO CONTENT")
-                if get_action_by_name(args["name"]):
+                if ActionModel.get_by_name(args["name"]):
                     return self.send(
                         response_type="CONFLICT",
                         additional_message=f"{args['name']} already exists ",
                     )
-                update_action(id_, args["name"])
+                ActionModel.update_action(id_, args["name"])
                 return self.send(response_type="NO CONTENT")
 
             return self.send(response_type="FORBIDDEN")

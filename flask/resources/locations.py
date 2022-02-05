c7ebea207f235820da4690b0553bf2616cfee512
daehan <dh16931@gmail.com>
2022-02-06 08:50:37 +0900

    refactor: action. location code

--- flask/resources/locations.py
+++ flask/resources/locations.py
@@ -1,11 +1,10 @@
 import traceback
-import sqlalchemy
 from flask_restplus import Namespace, reqparse, Resource
 
 from core.response import CustomeResponse
 from core.models import Location as LocationModel
-from core.database import db
 from core.response import exception_handler, login_required
+from core.utils import set_doc_responses
 
 api = Namespace("locations", description="locations related operations")
 
@@ -17,84 +16,78 @@ parser_search = reqparse.RequestParser()
 parser_search.add_argument("name", type=str, help="location name")
 
 
-def create_location(name):
-    try:
-        location = LocationModel(name)
-        location.create()
-        return location, ""
-    except sqlalchemy.exc.IntegrityError as e:
-        return False, f"Location name '{name}' already exists."
-
-
-def get_locations(name=None):
-    if name is not None:
-        locations = LocationModel.query.filter(LocationModel.name.like(f"%{name}%"))
-    else:
-        locations = LocationModel.query.all()
-    return [location.serialize for location in locations]
-
-
-def update_location(id_, name):
-    location = LocationModel.query.get(id_)
-    location.name = name
-    db.session.commit()
-
-
 parser_auth = reqparse.RequestParser()
 parser_auth.add_argument("Authorization", type=str, location="headers")
 
 
 @api.route("/")
 class Locations(Resource, CustomeResponse):
-    @api.doc("Get all locations")
+    @api.doc(responses=set_doc_responses(200, 500))
     @api.expect(parser_search)
     @exception_handler
     def get(self):
+        """Get all locations with filter by name if given."""
         args = parser_search.parse_args()
-        return self.send(response_type="OK", result=get_locations(name=args["name"]))
+        return self.send(
+            response_type="OK", result=LocationModel.get_all(name=args["name"])
+        )
 
-    @api.doc("create a new location")
+    @api.doc(responses=set_doc_responses(200, 401, 403, 409, 500))
     @api.expect(parser_post, parser_auth)
     @login_required
     @exception_handler
     def post(self, **kwargs):
+        """Create a new location."""
         if kwargs["auth_user"].is_admin():
             args = parser_post.parse_args()
-            result, message = create_location(args["name"])
+            result, message = LocationModel.create(args["name"])
             if result:
                 return self.send(response_type="CREATED", result=result.id)
-            return self.send(response_type="BAD REQUEST", additional_message=message)
+            return self.send(response_type="CONFLICT", additional_message=message)
 
         return self.send(response_type="FORBIDDEN")
 
 
 @api.route("/<int:id_>")
-@api.param("id_", "The location identifier")
 class Location(Resource, CustomeResponse):
+    @api.doc(
+        params={"id_": "The location identifier"},
+        responses=set_doc_responses(200, 404, 500),
+    )
     @exception_handler
     def get(self, id_):
+        """Get location by id value."""
         if location := LocationModel.get_by_id(id_):
             return self.send(response_type="OK", result=location.serialize)
         return self.send(response_type="NOT FOUND")
 
-    @api.doc("update location name")
+    @api.doc(responses=set_doc_responses(204, 401, 403, 404, 409, 500))
     @api.expect(parser_post, parser_auth)
     @login_required
     @exception_handler
     def put(self, id_, **kwargs):
-        if LocationModel.get_by_id(id_):
+        """Update the name of the location."""
+        if location := LocationModel.get_by_id(id_):
             if kwargs["auth_user"].is_admin():
                 args = parser_post.parse_args()
-                update_location(id_, args["name"])
+                if location.name == args["name"]:
+                    return self.send(response_type="NO CONTENT")
+                if LocationModel.get_by_name(args["name"]):
+                    return self.send(
+                        response_type="CONFLICT",
+                        additional_message=f"{args['name']} already exists ",
+                    )
+                LocationModel.update(id_, args["name"])
                 return self.send(response_type="NO CONTENT")
             return self.send(response_type="FORBIDDEN")
         return self.send(response_type="NOT FOUND")
 
-    @api.doc("delete a location")
+    @api.doc(responses=set_doc_responses(204, 401, 403, 404, 500))
     @api.expect(parser_auth)
     @login_required
     @exception_handler
     def delete(self, id_, **kwargs):
+        """Delete the location."""
         if LocationModel.get_by_id(id_):
             if kwargs["auth_user"].is_admin():
                 LocationModel.delete_by_id(id_)

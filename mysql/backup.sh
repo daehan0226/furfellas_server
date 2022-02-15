8235dc2195f03d9266e4aac686ef3035e0fe6b42
Daehan <dh16931@gmail.com>
2022-02-15 14:50:48 +0000

    chore: add backup condition

--- mysql/backup.sh
+++ mysql/backup.sh
@@ -1,5 +1,10 @@
 if [ -f ../.env ]; then
   export $(echo $(cat ../.env | sed 's/#.*//g'| xargs) | envsubst)
+else
+  echo "No ../.env file"
+  exit
 fi
 
-docker exec $MYSQL /usr/bin/mysqldump -uroot --password=$MYSQL_PASSWORD $MYSQL_DATABASE >  backup/$(date '+%Y-%m-%d_%H:%M:%S').sql
+if [ $MYSQL_BACKUP = "true" ]; then
+  docker exec $MYSQL /usr/bin/mysqldump -uroot --password=$MYSQL_PASSWORD $MYSQL_DATABASE >  backup/$(date '+%Y-%m-%d_%H:%M:%S').sql
+fi

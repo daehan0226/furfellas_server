987d9a9a7e990365878e6dc5a01ddb425a2e12aa
daehan <dh16931@gmail.com>
2022-02-16 10:15:15 +0900

    chore: guncorn port variable

--- flask/gunicorn.conf.py
+++ flask/gunicorn.conf.py
@@ -1,9 +1,11 @@
+import os
+
 # https://docs.gunicorn.org/en/stable/settings.html
-bind = '0.0.0.0:8080'
-worker_class = 'sync' # default = sync
-workers = 2 # default = 1
-loglevel = 'debug'
-accesslog = './logs/access.log'
-acceslogformat ="%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
-errorlog =  './logs/error.log'
-capture_output = 'True' # for python print
+bind = f'0.0.0.0:{os.getenv("FLASK_PORT")}'
+worker_class = "sync"  # default = sync
+workers = 2  # default = 1
+loglevel = "debug"
+accesslog = "./logs/access.log"
+acceslogformat = "%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
+errorlog = "./logs/error.log"
+capture_output = "True"  # for python print
